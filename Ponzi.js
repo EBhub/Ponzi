84264078f6f5789bb8d9da216dc249dbea1588e6
EB <44556455+EBhub@users.noreply.github.com>
2021-10-24 16:55:53 +0200

    Update Ponzi.js

--- Ponzi.js
+++ Ponzi.js
@@ -1,18 +1,3 @@
-/*
-Save this file in your node project directory
-This script requires nodeJS installed the web3 and ethersjs node packages as well.
-
-https://www.npmjs.com/package/ethers
-https://www.npmjs.com/package/web3
-
-So to run enter your private key below and run the script.
-npm install web3 ethers
-node ponzi.js
-
-it will run the compound every 10 minutes
-
-*/
-
 const Web3 = require("web3")
 const ethers = require("ethers");
 const { default: BigNumber } = require("bignumber.js");
